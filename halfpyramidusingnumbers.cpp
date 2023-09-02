638f0baec8e1ac699ef98586a0c818c1468d35cd
Riya rai <118008485+riya053@users.noreply.github.com>
2023-09-02 18:43:20 +0530

    Create halfpyramidusingnumbers.cpp

---
+++ halfpyramidusingnumbers.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cout<<"enter the value of n\n";
+    cin>>n;
+   
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=0;j<i;j++)
+        {   
+            
+            cout<<i;
+            
+        
+                }
+        cout<<endl;
+    }
+}
+
+output:
+
+enter the value of n
+7
+1
+22
+333
+4444
+55555
+666666
+7777777
