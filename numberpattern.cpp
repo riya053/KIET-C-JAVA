31c24a7681edfe7affb16b77ac20268a0ac7d3c4
Riya rai <118008485+riya053@users.noreply.github.com>
2023-09-02 20:54:41 +0530

    Create numberpattern.cpp

---
+++ numberpattern.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter the value of n \n";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=(n-i);j++)
+        {   
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++)
+        {
+            cout<<j<<" ";
+        }
+        
+        cout<<endl;
+    }
+    
+}
+
+output:
+
+Enter the value of n 
+5
+    1 
+   1 2
+  1 2 3
+ 1 2 3 4
+1 2 3 4 5
